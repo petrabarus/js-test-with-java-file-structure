47c34d2a2b38d194e0680b432806e6e63049f0cf
petra <Petra Barus>
2017-06-22 13:18:11 +0700

    Add test.

---
+++ src/js/path1/path2/path3/path4/path5/Rectangle.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class Rectangle extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
---
+++ test/js/path1/path2/path3/path4/path5/RectangleTest.jsx
@@ -0,0 +1,4 @@
+let assert = require('chai').assert,
+    path = require('path');
+
+import Rectangle from './../../../../../../../src/js/path1/path2/path3/path4/path5/Rectangle';
